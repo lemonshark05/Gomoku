33171b515aed3246d99db59e6ab8c9bdb0d00fd7
lemonshark <zmeng8@usfca.com>
2022-10-11 18:02:29 -0700

    Initial Commit

---
+++ Gomoku/GomokuApp.swift
@@ -0,0 +1,17 @@
+//
+//  GomokuApp.swift
+//  Gomoku
+//
+//  Created by lemonshark on 2022/10/11.
+//
+
+import SwiftUI
+
+@main
+struct GomokuApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

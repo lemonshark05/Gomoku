a225c42d765f01be794e9209ba1cd2886da0aef9
lemonshark <zmeng8@usfca.com>
2022-12-31 23:06:35 -0800

    apppicon

--- Gomoku/Models/AiGame.swift
+++ Gomoku/Models/AiGame.swift
@@ -8,5 +8,27 @@
 import Foundation
 
 class AiGame: ObservableObject {
+    @Published var steps:String
+
+    init() {
+        self.steps = ""
+    }
     
+    func stepsString(points: Array<Elements>)-> String {
+        steps = ""
+        points.forEach { p in
+            steps = steps + "_" + PDict[p.row]! + "\(p.col)"
+        }
+        return steps
+    }
+    
+    func aiPoint(){
+        
+    }
+//http://81.70.152.141:8080/?stepsString=_h8_j6_g7_i9_g8_f10
+//    &color=BLACK&level=HIGH&gameId=
+    func getJson(){
+        "http://81.70.152.141:8080/?stepsString="+steps
+        
+    }
 }
--- Gomoku/Models/Elements.swift
+++ Gomoku/Models/Elements.swift
@@ -32,10 +32,20 @@ class Elements: ObservableObject {
 var px: Int = 0
 var py: Int = 0
 
-let GMap = [ 0:"a", 1:"b", 2:"c", 3:"d", 4:"e", 5:"f", 6:"g", 7:"h", 8:"i", 9:"j", 10:"k", 11:"l", 12:"m", 13:"n", 14:"o"]
+//, 16:"p", 17:"q", 18:"r", 19:"t"
+let PDict:[Int:String] = [ 1:"a", 2:"b", 3:"c", 4:"d", 5:"e", 6:"f", 7:"g", 8:"h", 9:"i", 10:"j", 11:"k", 12:"l", 13:"m", 14:"n", 15:"o"]
+//, "p":16, "q":17, "r":18, "t":19
+let JDict:[String:Int] = [ "a":1, "b":2, "c":3, "d":4, "e":5, "f":6, "g":7, "h":8, "i":9, "j":10, "k":11, "l":12, "m":13, "n":14, "o":15]
 
 enum GameState{
     case black, white
+}
+
+//{"input": "h8_a1_i9_a2", "x": 9, "y": 9}
+struct JsonDate: Codable {
+    let input: String
+    let x: Int
+    let y: Int
 }
 
 extension UIScreen{
@@ -45,5 +55,5 @@ extension UIScreen{
     static let LineSpace = (sWidth)/16
     static let sideW = LineSpace/4
     static var buttonDiameter = 2 + LineSpace
-    static var lineLength = ((15 - 1) * LineSpace)
+    static var lineLength = (14 * LineSpace)
 }
